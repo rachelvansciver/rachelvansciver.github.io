bad53a926a86f39293a5becf36415633153f69dc
rachel <rvansciver@lamar.edu>
2021-12-02 22:11:49 -0600

    updated shell script and readme

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
-
 # build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
@@ -21,5 +14,3 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:rachelvansciver/vue-weather.git master:gh-pages
-
-cd -
